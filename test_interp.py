3d08da2e4049d006e173c95836236c5fd6be7e48
Justin Lange <well1912@gmail.com>
2016-09-10 09:19:57 -0700

    Test lookup honors lexical scoping

--- test_interp.py
+++ test_interp.py
@@ -1,3 +1,19 @@
+import pytest
+
+
+@pytest.fixture
+def test_env():
+    from interp import empty_env, env_extend
+    return env_extend({'a': 1},
+                      env_extend({'b': 2},
+                                 env_extend({'a': 0}, empty_env)))
+
+
 def test_env_lookup():
     from interp import empty_env, env_lookup, env_extend
     assert env_lookup(env_extend({'a': 0}, empty_env), 'a') == 0
+
+
+def test_env_lookup_lexical(test_env):
+    from interp import env_lookup
+    assert env_lookup(test_env, 'a') == 1
