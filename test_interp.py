7d39b3248f2717102eea50d8cdd08a9c45c405cd
Justin Lange <well1912@gmail.com>
2016-09-10 09:12:00 -0700

    Have empty_env be a value

--- test_interp.py
+++ test_interp.py
@@ -1,12 +1,3 @@
-import pytest
-
-
-@pytest.fixture
-def empty_env():
-    from interp import env
-    return env(None)
-
-
-def test_env_lookup(empty_env):
-    from interp import env_lookup, env_extend
+def test_env_lookup():
+    from interp import empty_env, env_lookup, env_extend
     assert env_lookup(env_extend({'a': 0}, empty_env), 'a') == 0
