50f549313eaff01631b1b4e3d7e533b2248fbb47
Justin Lange <well1912@gmail.com>
2016-09-11 11:54:15 -0700

    Implement parse for simple tokens

--- parse.py
+++ parse.py
@@ -1,3 +1,9 @@
 def lex(s):
     """Lex a string into a list of tokens."""
     return s.replace('(', ' ( ').replace(')', ' ) ').split()
+
+
+def parse(tokens):
+    """Turn a list of tokens into a tree of symbols and numbers."""
+    t = tokens[0]
+    return parse_list(t) if t == '(' else t
