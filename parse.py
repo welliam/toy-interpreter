7e4ec3a978bdd5be310755b4982124bb74c330ea
Justin Lange <well1912@gmail.com>
2016-09-11 13:03:20 -0700

    Complete parse definition

--- parse.py
+++ parse.py
@@ -3,7 +3,21 @@ def lex(s):
     return s.replace('(', ' ( ').replace(')', ' ) ').split()
 
 
+def parse_list(tokens, i):
+    res = []
+    while True:
+        t = tokens[i]
+        if t == '(':
+            exp, i = parse_list(tokens, i+1)
+            res.append(exp)
+        elif t == ')':
+            return res, i + 1
+        else:
+            res.append(t)
+            i += 1
+
+
 def parse(tokens):
     """Turn a list of tokens into a tree of symbols and numbers."""
     t = tokens[0]
-    return parse_list(t) if t == '(' else t
+    return parse_list(tokens, 1)[0] if t == '(' else t
