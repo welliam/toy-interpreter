38e62c835b274f4350e7bef75bb827d557602c05
Justin Lange <well1912@gmail.com>
2016-09-11 11:51:48 -0700

    Implement tests for parse

--- test_parse.py
+++ test_parse.py
@@ -13,8 +13,24 @@ LEX_RESULTS = [
 ]
 
 
+PARSE_RESULTS = [
+    (['(', ')'], []),
+    (['x'], 'x'),
+    (['x', 'y'], 'x'),
+    (['(', 'x', 'y', ')'], ['x', 'y']),
+    (['(', 'x', '(', 'y', ')', ')'], ['x', ['y']])
+]
+
+
 @pytest.mark.parametrize('string, tokens', LEX_RESULTS)
 def test_lex(string, tokens):
     """Test lex."""
     from parse import lex
     assert lex(string) == tokens
+
+
+@pytest.mark.parametrize('tokens, tree', PARSE_RESULTS)
+def test_parse(tokens, tree):
+    """Test parse."""
+    from parse import parse
+    assert parse(tokens) == tree
