9d52b372211152b7aa83f0d2b0a4fe6ed1b3eeb8
Justin Lange <well1912@gmail.com>
2016-09-11 18:59:11 -0700

    Fix evaluate_set test dict

--- test_interp.py
+++ test_interp.py
@@ -214,6 +214,6 @@ def test_evaluate_if_false(fresh_env):
 
 def test_set_sets_var():
     from interp import make_env, evaluate_set, env_lookup
-    env = make_env({'a', 0})
+    env = make_env({'a': 0})
     evaluate_set(['a', 1], env)
     assert env_lookup(env, 'a') == 1
