b3210db67ed1e5eeedce138a76180579726558cc
Justin Lange <well1912@gmail.com>
2016-09-11 18:47:28 -0700

    Make parse_list iterative
    * more pythonic
    * won't blow the stack
    * not that the interpreter is iterative anyway

--- parse.py
+++ parse.py
@@ -16,23 +16,27 @@ def parse_list(tokens, i):
     """Parse a list out of the tokens.
 
     Returns the list and an index of where that list ends."""
+    todo = []
     res = []
     while True:
         t = tokens[i]
         if t == '(':
-            exp, i = parse_list(tokens, i+1)
-            res.append(exp)
+            todo.append(res)
+            res = []
         elif t == ')':
-            return res, i + 1
+            if todo:
+                res = todo.pop() + [res]
+            else:
+                return res
         else:
             res.append(t)
-            i += 1
+        i += 1
 
 
 def parse(tokens):
     """Turn a list of tokens into a tree of symbols and numbers."""
     t = tokens[0]
-    return parse_list(tokens, 1)[0] if t == '(' else t
+    return parse_list(tokens, 1) if t == '(' else t
 
 
 def read_string(s):
