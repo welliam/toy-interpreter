b36ab9b6945e543e1d6732eb434bea0c833a5801
Justin Lange <well1912@gmail.com>
2016-09-14 09:08:47 -0700

    Test to string methods

--- test_interp.py
+++ test_interp.py
@@ -34,3 +34,38 @@ def test_unbalanced(s):
     from interp import balanced
     with pytest.raises(ValueError):
         balanced(s)
+
+def test_cons_str_simple():
+    from interp import cons
+    assert str(cons(1, 2)) == '(1 . 2)'
+
+
+def test_cons_str_embedded():
+    from interp import cons
+    assert str(cons(cons(1, 2), cons(1, 2))) == '((1 . 2) . (1 . 2))'
+
+
+def test_cons_str_linked():
+    from interp import cons
+    assert str(cons(1, cons(2, 3))) == '(1 . (2 . 3))'
+
+
+def test_list_str_empty():
+    from interp import null
+    assert str(null) == '()'
+
+
+def test_list_str_simple():
+    from interp import cons, null
+    assert str(cons(1, null)) == '(1)'
+
+
+def test_list_str_long():
+    from interp import cons, null
+    assert str(cons(1, (cons(2, cons(3, null))))) == '(1 2 3)'
+
+
+def test_list_str_embedded():
+    from interp import cons, null
+    lst = cons(cons(1, cons(2, null)), cons(3, cons(4, null)))
+    assert str(lst) == '((1 2) 3 4)'
